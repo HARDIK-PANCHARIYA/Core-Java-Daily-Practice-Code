1ab5c3b9389b73c233e10d15f3064b4ff96f8d8e
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-06-16 23:00:41 +0530

    day 3 practice

---
+++ src/com/core/day3/calculator.java
@@ -0,0 +1,31 @@
+package com.core.day3;
+
+public class calculator {
+	
+	public static void main(String[] args) {
+		
+		int A = 10;
+		int B = 20;
+		
+		//Addition 		
+		int sum = A+B;
+		//Subtraction
+		int sub = A-B;
+		//Multiplication
+		int mul = A*B;
+		//Division
+		double div = (double) A/B;
+		
+		
+		System.out.println("Addition of A + B = "+sum);
+		
+		System.out.println("Subtraction of A - B = "+ sub);
+		
+		System.out.println("Multiplication of A * B = "+mul);
+		
+		System.out.println("Division of A / B = "+div);
+		
+		
+	}
+
+}
