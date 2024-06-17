b19d9afe2774f93d9098025daab8da7d919e0ad3
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-06-17 18:43:21 +0530

    day 4 practice

---
+++ src/com/core/java/helloworld.java
@@ -0,0 +1,16 @@
+package com.core.java;
+
+public class helloworld {
+	
+	public static void main(String[] args) {
+		System.err.println("Hello World");
+		
+		System.out.println("Additin of two number 10+20 = "+(10+20));
+		
+		System.out.println("Substraction of two  number 10-20 = "+(10-20));
+		
+		System.out.println("Multplication of two number 10*20 = "+(10*20));
+		
+	}
+
+}
