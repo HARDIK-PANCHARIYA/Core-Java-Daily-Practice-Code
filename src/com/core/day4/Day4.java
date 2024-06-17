f05ed25b0cb93512f2f8d8a43bb85ed85a334f4a
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-06-17 18:58:12 +0530

    day 4 Debug line my line

---
+++ src/com/core/day4/Day4.java
@@ -0,0 +1,13 @@
+package com.core.day4;
+
+public class Day4 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Addition of two no 300 + 200 = "+(300+200));
+		
+		System.out.println("Subtraction of two no 300 - 100 = "+(300-100));
+		
+		System.out.println("Multiplication of two no 30 * 20 = "+(30*20));
+	}
+}
