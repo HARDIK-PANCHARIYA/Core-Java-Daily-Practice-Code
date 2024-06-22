bcf646c5978a6adba344b9ae0fe82668655ab166
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-06-22 20:02:07 +0530

    day 7 default data type values

---
+++ src/com/core/day8/Datatypedefaultvalue.java
@@ -0,0 +1,18 @@
+package com.core.day8;
+
+public class Datatypedefaultvalue {
+	
+	
+	static byte sa;
+	
+	byte a;
+	int b;
+	float c;
+	double d;
+	char e;
+	long f;
+	boolean g;
+	short h;
+	
+
+}
---
+++ src/com/core/day8/Datatypedefaultvaluetest.java
@@ -0,0 +1,24 @@
+package com.core.day8;
+
+public class Datatypedefaultvaluetest {
+	
+	public static void main(String[] args) {
+		
+	Datatypedefaultvalue obj=new Datatypedefaultvalue();
+	
+	
+	System.out.println("static byte = "+obj.sa);
+	
+	
+	System.out.println("byte = "+obj.a);
+	System.out.println("int = "+obj.b);
+	System.out.println("float = "+obj.c);
+	System.out.println("double = "+obj.d);
+	System.out.println("char = "+obj.e+"@"); //default value space
+	System.out.println("long = "+obj.f);
+	System.out.println("boolean = "+obj.g);
+	System.out.println("short = "+obj.h);
+	
+
+	}
+}
