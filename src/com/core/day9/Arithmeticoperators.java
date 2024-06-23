48f0e7125602de80121ed1d824b74a5316490a7a
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-06-23 13:17:57 +0530

    day 9 Arithmetic Operators

---
+++ src/com/core/day9/Arithmeticoperators.java
@@ -0,0 +1,22 @@
+package com.core.day9;
+
+public class Arithmeticoperators {
+	
+	void Arithmeticoperators(float a, float b) {
+		
+		float add = a+b;
+		float sub = a-b;
+		float mul = a*b;
+		float div = a/b;
+		float mod = a%b;
+		
+		System.out.println("Addition = "+add);
+		System.out.println("Substraction = "+sub);
+		System.out.println("Multplication = "+mul);
+		System.out.println("Division = "+div);
+		System.out.println("Modulas = "+mod);
+		
+		
+	}
+
+}
---
+++ src/com/core/day9/Arithmeticoperatorstest.java
@@ -0,0 +1,12 @@
+package com.core.day9;
+
+public class Arithmeticoperatorstest {
+	
+	public static void main(String args[]) {
+		
+		Arithmeticoperators ar1 = new Arithmeticoperators();
+		
+		ar1.Arithmeticoperators(10f, 20f);
+	}
+
+}
