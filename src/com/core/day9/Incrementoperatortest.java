3f2b4eae3328acb3c0cb6e00e86b18d67d8394bd
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-06-23 12:47:32 +0530

    day 9 operator

---
+++ src/com/core/day9/Incrementoperatortest.java
@@ -0,0 +1,17 @@
+package com.core.day9;
+
+public class Incrementoperatortest {
+	
+	public static void main (String args[]) {
+		
+		Incrementoperator pre1=new Incrementoperator();
+		
+		Incrementoperator post1=new Incrementoperator();
+		
+		pre1.Preincrement();
+		
+		post1.Postincrement();
+		
+	}
+
+}
