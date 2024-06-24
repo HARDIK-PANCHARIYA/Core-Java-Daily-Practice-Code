84eca6b6e2f82b13c0c48aea132ea896c8652c8b
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-06-24 18:59:29 +0530

    day 9 Relational Operators

---
+++ src/com/core/day9/Relationaloperator.java
@@ -0,0 +1,22 @@
+package com.core.day9;
+
+
+public class Relationaloperator {
+	
+	int a = 10;
+	int b = 20;
+	
+	void Relationaloperators() {
+		
+		System.out.println("a = "+a+"  "+"b = "+b);
+		System.out.println("a is Grater than b ? = "+ (a>b));
+		System.out.println("a is Smaller than b ? = "+(a<b));
+		System.out.println("a is Grater than or Equal to  b ? = "+(a>=b));
+		System.out.println("a is Smaller than or Equal to  b ? = "+(a<=b));
+		System.out.println("a is equal to b ? = "+(a==b));
+		System.out.println("a is not equal to b ? = "+(a!=b));
+		
+		
+	}
+
+}
---
+++ src/com/core/day9/Relationaloperatortest.java
@@ -0,0 +1,13 @@
+package com.core.day9;
+
+public class Relationaloperatortest {
+	public static void main(String[] args) {
+		
+	
+	Relationaloperator r1=new Relationaloperator();
+	
+	r1.Relationaloperators();
+		
+	}
+
+}
