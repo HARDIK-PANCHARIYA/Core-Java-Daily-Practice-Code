c30361afbc49dec45f5e1f1eedce1415816da359
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-06-24 19:03:36 +0530

    day 10 Pattern

---
+++ src/com/core/day10/A.java
@@ -0,0 +1,21 @@
+package com.core.day10;
+
+public class A {
+	
+	public static void main(String[] args) {
+		
+		for (int i=1;i<=5;++i) {
+			
+			for(int j=1;j<=5;j++) {
+				
+				System.out.print("("+i+","+j+") ");
+				}
+			
+			
+			System.out.println();
+		}
+	}
+		
+	
+
+}
