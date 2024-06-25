340892c80458381088b0903b5361e32da39b0e6e
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-06-25 14:52:08 +0530

    day 10 Pattern

---
+++ src/com/core/day10/pattern4.java
@@ -0,0 +1,30 @@
+package com.core.day10;
+
+//@@@@@
+//@***@
+//@***@
+//@***@
+//@@@@@
+
+public class pattern4 {
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=5; i++) {
+			
+			for(int j=1; j<=5; j++) {
+				
+				if(i==1 || i==5 || j==1 || j==5)
+					
+					System.out.print("@");
+				
+				else
+				
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
