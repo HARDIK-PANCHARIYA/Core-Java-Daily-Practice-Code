492332305c07c507b5415727656dc1b2fe597f3d
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-06-25 14:52:35 +0530

    day 10 Pattern

---
+++ src/com/core/day10/Pattern5.java
@@ -0,0 +1,30 @@
+package com.core.day10;
+
+//@@@@@
+//*****
+//@@@@@
+//*****
+//@@@@@
+
+public class Pattern5 {
+	
+	public static void main(String[] args) {
+		
+		for (int i=1; i<=5; i++) {
+			
+			for (int j=1; j<=5; j++) {
+				
+				if(i==1 || i==3 || i==5) {
+					System.out.print("@");
+				}
+				else {
+				System.out.print("*");
+				}
+				
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
