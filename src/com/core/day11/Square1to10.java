de9de612ff7b7a20a57acb5b5b5c26d5f9d563d8
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-06-27 20:29:53 +0530

    day 11 For loop uses

---
+++ src/com/core/day11/Square1to10.java
@@ -0,0 +1,13 @@
+package com.core.day11;
+
+public class Square1to10 {
+	
+	public static void main(String args[]) {
+		
+		for(int i=1;i<=10;i++) {
+			System.out.println(i+" Square = "+(i*i));
+		}
+		
+	}
+
+}
