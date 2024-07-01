99880616814b7e3c5cbbc09392314e2aa48417a0
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-01 17:43:11 +0530

    day 11 For loop uses

---
+++ src/com/core/day11/Test4.java
@@ -0,0 +1,16 @@
+package com.core.day11;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=5; i++) {
+			
+			for(int j=69; j>=65; j--) {
+				System.out.print((char)j+" ");
+			}
+			System.out.println();
+			
+		}
+	}
+}
