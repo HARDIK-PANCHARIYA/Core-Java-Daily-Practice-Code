8182555b1fac687e8f30e1c6185f4bbf257ec772
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-02 19:08:47 +0530

    day 12 Pattern

---
+++ src/com/core/day12/Test2.java
@@ -0,0 +1,20 @@
+package com.core.day12;
+
+/*
+ 	11
+ 	111
+ 	1111
+ 	11111
+ 	111111
+ */
+public class Test2 {
+	
+	public static void main(String args[]) {
+		
+		for(int i=11;i<=111111;i=i*10+1) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
