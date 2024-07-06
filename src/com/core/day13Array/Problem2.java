73cde4bcc680b4c60e607e194341aeaf2ca561c8
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-06 21:20:30 +0530

    day 13 array

---
+++ src/com/core/day13Array/Problem2.java
@@ -0,0 +1,19 @@
+package com.core.day13Array;
+
+//   Creating of array  method 2
+
+public class Problem2 {
+	
+	public static void main(String[] args) {
+		
+
+	int a[]= {10,20,30,40};
+	
+	for(int i=0; i<a.length; i++) {
+		
+		System.out.println(a[i]);
+	}
+	
+	}
+
+}
