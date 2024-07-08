7f5218d513c177dbdae7e66e33cf3618413e2fac
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-08 23:19:38 +0530

    day 13 array

---
+++ src/com/core/day13Array/Problem12.java
@@ -0,0 +1,19 @@
+package com.core.day13Array;
+
+//  Increase each Element of array by 5
+
+public class Problem12 {
+	
+	public static void main(String[] args) {
+		
+		int a[]= {10,20,30,40,50,60};
+		
+		for(int i=0; i<a.length; i++) {
+			
+			System.out.println(a[i]+5);
+			
+		}
+		
+	}
+
+}
