03d4d62f069b829577a8eae4c440b34e8488fe90
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-08 23:19:09 +0530

    day 13 array

---
+++ src/com/core/day13Array/Problem11.java
@@ -0,0 +1,19 @@
+package com.core.day13Array;
+
+//	Print content of array in reverse order
+
+public class Problem11 {
+	
+	public static void main(String[] args) {
+		
+
+	int a[]= {12,4655,89,21,35,78,154,15};
+	
+	for(int i=a.length-1; i>=0; i--) {
+		
+		System.out.println(a[i]);
+		
+	}
+
+	}
+}
