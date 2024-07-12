9322b021c6f8596bf692690a6ff22041a73ffe9b
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-12 19:15:14 +0530

    day 15 String

---
+++ src/com/core/day15String/Stringtest6.java
@@ -0,0 +1,22 @@
+package com.core.day15String;
+
+public class Stringtest6 {
+	
+	public static void main(String[] args) {
+			
+//		int a1[]= {10,20,30,40,50};
+//		
+//		System.out.println(a1[4]);
+		
+		int a[]= new int[3];
+		
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		
+		System.out.println(a[2]);
+		
+		
+	}
+
+}
