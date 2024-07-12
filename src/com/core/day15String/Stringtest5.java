b66ee647d7b076adaf70beea4cb3d35673366f3c
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-12 19:15:00 +0530

    day 15 String

---
+++ src/com/core/day15String/Stringtest5.java
@@ -0,0 +1,21 @@
+package com.core.day15String;
+
+// To get the perfect index no of particular letter
+
+// use CharAt ---> to get perfect 
+
+// use toUpperCase ---> to hole string to upper case
+
+public class Stringtest5 {
+	
+	public static void main(String[] args) {
+		
+		String s1= "Hardik Panchariya";
+		
+		System.out.println(s1.charAt(4));
+		
+		System.out.println(s1.toUpperCase());
+		
+	}
+
+}
