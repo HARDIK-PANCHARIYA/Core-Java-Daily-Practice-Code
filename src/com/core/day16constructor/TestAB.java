14f7b1ca27da2ac688e495e767b92f122d59dca7
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-13 23:22:56 +0530

    day 16 constructor

---
+++ src/com/core/day16constructor/TestAB.java
@@ -0,0 +1,11 @@
+package com.core.day16constructor;
+
+public class TestAB {
+	
+	public static void main(String[] args) {
+		A a1=new A();
+		System.out.println("----------------------------------------");
+		B obj=new B();
+	}
+
+}
