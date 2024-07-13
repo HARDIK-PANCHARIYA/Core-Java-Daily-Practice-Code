62352684f0c22d7030037a61b3cec569670197f0
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-13 23:25:20 +0530

    day 16 constructor

---
+++ src/com/core/day16constructor/TestConstructor.java
@@ -0,0 +1,20 @@
+package com.core.day16constructor;
+
+public class TestConstructor {
+	
+	public static void main(String[] args) {
+		
+	
+	
+	Constructor c1=new Constructor();
+	Constructor c2=new Constructor(30);
+	Constructor c3=new Constructor(40,50);
+	
+	c1.dispaly();
+	c2.dispaly();
+	c3.dispaly();
+	
+	}
+	
+
+}
