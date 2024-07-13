b3e0068754f261e45403a8fbfaeecba908fcf204
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-13 23:22:27 +0530

    day 16 constructor

---
+++ src/com/core/day16constructor/B.java
@@ -0,0 +1,15 @@
+package com.core.day16constructor;
+
+public class B extends A {
+	
+	int x=110;
+	
+	B(){
+		System.out.println("Cons of B ");
+	}
+	
+	void m2() {
+		System.out.println(x);
+	}
+
+}
