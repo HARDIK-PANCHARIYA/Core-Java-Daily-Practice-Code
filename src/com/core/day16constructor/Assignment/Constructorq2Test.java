660aa948b7f0c60ebcaaa711b986f72f019cc9ec
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-15 23:26:52 +0530

    day 16 constructor Assignment q2

---
+++ src/com/core/day16constructor/Assignment/Constructorq2Test.java
@@ -0,0 +1,12 @@
+package com.core.day16constructor.Assignment;
+
+public class Constructorq2Test {
+	
+	public static void main(String[] args) {
+		
+		Constructorq2 a1= new Constructorq2();
+		Constructorq2 a2=new Constructorq2(10);
+		
+	}
+
+}
