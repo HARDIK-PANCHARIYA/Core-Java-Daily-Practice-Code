2c52e6b6b5a0c7dc9474c386e35bce0a0f7d3560
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-15 23:26:25 +0530

    day 16 constructor Assignment q2

---
+++ src/com/core/day16constructor/Assignment/Constructorq2.java
@@ -0,0 +1,13 @@
+package com.core.day16constructor.Assignment;
+
+public class Constructorq2 {
+	
+	Constructorq2(){
+		System.out.println("Constructor Running Default or Non Parameterized");
+	}
+	
+	Constructorq2(int a){
+		System.out.println("Constructor Running Parameterizedn"+a);
+	}
+
+}
