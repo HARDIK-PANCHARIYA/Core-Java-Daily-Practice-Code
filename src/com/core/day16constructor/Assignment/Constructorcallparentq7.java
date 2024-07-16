0cde3446b14348864f17fc9e7fe5d36f1baceb0f
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-16 23:40:11 +0530

    day 16 constructor Assignment q7

---
+++ src/com/core/day16constructor/Assignment/Constructorcallparentq7.java
@@ -0,0 +1,9 @@
+package com.core.day16constructor.Assignment;
+
+public class Constructorcallparentq7 {
+	
+	public Constructorcallparentq7(){
+		System.out.println("Parent class constructor");
+	}
+
+}
