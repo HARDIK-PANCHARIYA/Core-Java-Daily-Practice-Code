9c27a8ce27f2620100a9579b660992e6454f8722
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-16 23:39:13 +0530

    day 16 constructor Assignment q6

---
+++ src/com/core/day16constructor/Assignment/Chainingq6test.java
@@ -0,0 +1,11 @@
+package com.core.day16constructor.Assignment;
+
+public class Chainingq6test {
+	
+	public static void main(String[] args) {
+		
+		Chainingq6 obj=new Chainingq6(30,40,50);
+		
+	}
+
+}
