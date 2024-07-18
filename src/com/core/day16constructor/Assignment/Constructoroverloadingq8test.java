6ec91f7798da3aa6f3b2a3b36026e2196365e27b
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-18 23:44:31 +0530

    day 16 constructor Assignment q8

---
+++ src/com/core/day16constructor/Assignment/Constructoroverloadingq8test.java
@@ -0,0 +1,12 @@
+package com.core.day16constructor.Assignment;
+
+public class Constructoroverloadingq8test {
+	
+	public static void main(String[] args) {
+	Constructoroverloadingq8 obj=new Constructoroverloadingq8(11);
+	System.out.println(obj.getNum());
+	System.out.println("=======================================");
+	obj.setNum(22);
+	System.out.println(obj.getNum());
+	}
+}
