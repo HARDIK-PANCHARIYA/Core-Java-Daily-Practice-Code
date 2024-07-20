a240b834459da8469720221cc743bd7948aa948d
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-20 22:37:54 +0530

    Day 18 Encapsulation Assignment

---
+++ src/com/core/day18encapsulation/assignment/Areaq1test.java
@@ -0,0 +1,11 @@
+package com.core.day18encapsulation.assignment;
+
+public class Areaq1test {
+	
+	public static void main(String[] args) {
+		
+	Areaq1 a1=new Areaq1(5,6);
+	a1.Rectangearea();
+	
+}
+}
