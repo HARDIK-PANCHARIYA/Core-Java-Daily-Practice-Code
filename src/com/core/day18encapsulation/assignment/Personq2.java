be7a6f6acd58f038b8cff682f4891fed06ecc3fa
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-20 22:39:37 +0530

    Day 18 Encapsulation Assignment

---
+++ src/com/core/day18encapsulation/assignment/Personq2.java
@@ -0,0 +1,22 @@
+package com.core.day18encapsulation.assignment;
+
+public class Personq2 {
+	
+	private int age;
+	
+	int getAge() {
+		return age;
+	}
+	
+	void setAge(int age) {
+		this.age=age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [age=" + age + "]";
+	}
+	
+	
+
+}
