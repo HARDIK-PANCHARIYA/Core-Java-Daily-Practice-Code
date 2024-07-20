08b2218bed4ab8bd1ba185c15c2979bd456e4c93
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-20 22:37:22 +0530

    Day 18 Encapsulation Assignment

---
+++ src/com/core/day18encapsulation/assignment/Areaq1.java
@@ -0,0 +1,23 @@
+package com.core.day18encapsulation.assignment;
+
+public class Areaq1 {
+	
+	int length;
+	int breath;
+	
+	Areaq1(int length, int breath){
+		super();
+		this.length=length;
+		this.breath=breath;
+	}
+	
+
+	void Rectangearea() {
+		int area=length*breath;
+		System.out.println("Area of rectange = "+area);
+	}
+	
+	
+	
+
+}
