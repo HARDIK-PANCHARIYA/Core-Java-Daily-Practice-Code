d7cc4f444c3054882aa45572d7f0e188ef8e2741
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-20 22:40:50 +0530

    Day 18 Encapsulation Assignment

---
+++ src/com/core/day18encapsulation/assignment/Studentq3test.java
@@ -0,0 +1,14 @@
+package com.core.day18encapsulation.assignment;
+
+public class Studentq3test {
+	
+	public static void main(String[] args) {
+		
+		Studentq3 s1=new Studentq3();
+		s1.setName("Hardik");
+		
+		System.out.println("Name == "+s1.getName());
+		
+	}
+
+}
