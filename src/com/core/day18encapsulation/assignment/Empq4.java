2491254f26b8ce366c99660de057e9a73ee497fb
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-21 23:20:58 +0530

    Day 18 Encapsulation Assignment

---
+++ src/com/core/day18encapsulation/assignment/Empq4.java
@@ -0,0 +1,32 @@
+package com.core.day18encapsulation.assignment;
+
+public class Empq4 {
+	
+	private int id;
+	private String empname;
+	private int empage;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String emp) {
+		this.empname=emp;
+	}
+	
+	
+	public int getEmpage() {
+		return empage;
+	}
+	public void setEmpage(int age) {
+		this.empage=age;
+	}
+}
