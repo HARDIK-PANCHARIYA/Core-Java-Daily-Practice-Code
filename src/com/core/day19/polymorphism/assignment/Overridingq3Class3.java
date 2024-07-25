662fce1e2b17381190111323f8807ae78c6ef623
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-25 22:12:53 +0530

    Day 19 polymorphism assignment Overridingq3

---
+++ src/com/core/day19/polymorphism/assignment/Overridingq3Class3.java
@@ -0,0 +1,12 @@
+package com.core.day19.polymorphism.assignment;
+
+public class Overridingq3Class3 extends Overridingq3Class2 {
+
+	void display() {
+		super.display();
+		
+		s1="Class 3";
+		System.out.println(s1+" function ");
+	}
+	
+}
