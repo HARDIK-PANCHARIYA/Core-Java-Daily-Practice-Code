cea8bcaca532a087668b1f1b80421de89c5da8e1
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-26 23:41:55 +0530

    Day 19 polymorphism assignment Overridingq1DerivedClass

---
+++ src/com/core/day19/polymorphism/assignment/Overridingq1DerivedClass.java
@@ -0,0 +1,9 @@
+package com.core.day19.polymorphism.assignment;
+
+public class Overridingq1DerivedClass extends Overridingq1baseClass {
+	
+	public void method() {
+		System.out.println("Derived class");
+	}
+
+}
