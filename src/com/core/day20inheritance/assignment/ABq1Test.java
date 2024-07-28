3137d166a21b195b4a3658e2804cb5eb42a62e58
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-28 22:27:02 +0530

    Day 20 polymorphism assignmeinheritance q1Test

---
+++ src/com/core/day20inheritance/assignment/ABq1Test.java
@@ -0,0 +1,14 @@
+package com.core.day20inheritance.assignment;
+
+public class ABq1Test {
+	
+	public static void main(String[] args) {
+		
+		Bq1 obj =new Bq1();
+		
+		obj.methodB();
+		obj.methodA();
+		
+	}
+
+}
