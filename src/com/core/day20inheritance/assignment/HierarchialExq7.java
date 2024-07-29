d45cea2fef71a0424fde4a2a672694c6e9b62373
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-29 23:39:00 +0530

    Day 20 polymorphism assignment inheritance q7

---
+++ src/com/core/day20inheritance/assignment/HierarchialExq7.java
@@ -0,0 +1,16 @@
+package com.core.day20inheritance.assignment;
+
+public class HierarchialExq7 {
+	
+	public static void main(String[] args) {
+		Bq7 obj1=new Bq7();
+		Cq7 obj2=new Cq7();
+		Dq7 obj3=new Dq7();
+		
+		obj1.methodA();
+		obj2.methodA();
+		obj3.methodA();
+		
+	}
+
+}
