2d377b5b6a3f4ac82c917500d671a73f6cf1be35
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-30 22:52:47 +0530

    Day 20 polymorphism assignment inheritance q5

---
+++ src/com/core/day20inheritance/assignment/Carq5.java
@@ -0,0 +1,13 @@
+package com.core.day20inheritance.assignment;
+
+public class Carq5 {
+	
+	public Carq5() {
+		System.out.println("Constructor of car class");
+	}
+	
+	public void vehicalType() {
+		System.out.println("Vehivcal type = car");
+	}
+
+}
