b6fe916b2c4f1aba0da6920cee08a7ba9068dac2
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-30 22:55:08 +0530

    Day 20 polymorphism assignment inheritance q5

---
+++ src/com/core/day20inheritance/assignment/Testq5.java
@@ -0,0 +1,14 @@
+package com.core.day20inheritance.assignment;
+
+public class Testq5 {
+	
+	public static void main(String[] args) {
+		
+		Swiftq5 s1= new Swiftq5();
+		
+		s1.vehicalType();
+		s1.speed();
+		s1.brand();
+	}
+
+}
