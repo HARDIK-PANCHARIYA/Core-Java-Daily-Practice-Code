963020c47c5a8f39a7976d75dad2f61637f9c38e
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-30 22:52:10 +0530

    Day 20 polymorphism assignment inheritance q3

---
+++ src/com/core/day20inheritance/assignment/Vehicalq3.java
@@ -0,0 +1,33 @@
+package com.core.day20inheritance.assignment;
+
+public class Vehicalq3 {
+	
+	String color;
+	private int speed;
+	private int size;
+	
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color=color;
+	}
+	
+	
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed=speed;
+	}
+	
+	
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size=size;
+	}
+
+}
