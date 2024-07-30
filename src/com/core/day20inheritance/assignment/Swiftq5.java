5ad6155f33c7d762619edce5b53d80bc782402e4
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-30 22:54:09 +0530

    Day 20 polymorphism assignment inheritance q5

---
+++ src/com/core/day20inheritance/assignment/Swiftq5.java
@@ -0,0 +1,13 @@
+package com.core.day20inheritance.assignment;
+
+public class Swiftq5 extends Marutiq5{
+	
+	public Swiftq5() {
+		System.out.println("constructor of Swift");
+	}
+	
+	public void speed() {
+		System.out.println("Max speed = 70kmph");
+	}
+
+}
