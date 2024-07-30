6ddda424684d585c6359a28a45d0d087f01a640c
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-30 22:53:32 +0530

    Day 20 polymorphism assignment inheritance q5

---
+++ src/com/core/day20inheritance/assignment/Marutiq5.java
@@ -0,0 +1,17 @@
+package com.core.day20inheritance.assignment;
+
+public class Marutiq5 extends Carq5{
+
+	public Marutiq5() {
+		System.out.println("Constructor of Maruti");
+	}
+	
+	public void brand() {
+		System.out.println("Brand = Maruti");
+	}
+	
+	public void speed() {
+		System.out.println("Max speed = 90kmph");
+	}
+
+}
