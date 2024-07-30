d10786a99b7415e943dc97fb95ef5364d44c0583
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-30 22:51:18 +0530

    Day 20 polymorphism assignment inheritance q3

---
+++ src/com/core/day20inheritance/assignment/Carq3.java
@@ -0,0 +1,33 @@
+package com.core.day20inheritance.assignment;
+
+public class Carq3 extends Vehicalq3{
+	
+	int cc;
+	int gear;
+	String color;
+	
+	
+	public int getCc() {
+		return cc;
+	}
+	public void setCc(int cc) {
+		this.cc=cc;
+	}
+	
+	
+	public int getGear() {
+		return gear;
+	}
+	public void setGear(int gear) {
+		this.gear=gear;
+	}
+	
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color=color;
+	}
+
+}
