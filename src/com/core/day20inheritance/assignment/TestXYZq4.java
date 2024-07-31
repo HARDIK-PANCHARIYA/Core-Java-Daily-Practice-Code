37898751a5242238ca8e49cb8717b2d07b9ef093
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-07-31 22:59:18 +0530

    Day 20 polymorphism assignment inheritance q4

---
+++ src/com/core/day20inheritance/assignment/TestXYZq4.java
@@ -0,0 +1,15 @@
+package com.core.day20inheritance.assignment;
+
+public class TestXYZq4 {
+	
+	public static void main(String[] args) {
+		
+	Zq4 obj=new Zq4();
+	
+	obj.methodX();
+	obj.methodY();
+	obj.methodZ();
+	
+	
+	}
+}
