086b4abd50d188dc4401e43b6c62039a364212c5
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-08-05 23:10:45 +0530

    Day 20 assignment inheritance q9

---
+++ src/com/core/day20inheritance/assignment/ProtectedVariableDemoq9.java
@@ -0,0 +1,12 @@
+package com.core.day20inheritance.assignment;
+
+import com.core.day20inheritance.assignment.test.Squareq9;
+
+public class ProtectedVariableDemoq9 {
+	public static void main(String[] args) {
+		Squareq9 obj=new Squareq9(10);
+		obj.printSide();
+	}
+	
+
+}
