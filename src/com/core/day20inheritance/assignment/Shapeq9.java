f130d97a1b98cdf4a8b483a313ad42db491cd045
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-08-05 23:11:15 +0530

    Day 20 assignment inheritance q9

---
+++ src/com/core/day20inheritance/assignment/Shapeq9.java
@@ -0,0 +1,19 @@
+package com.core.day20inheritance.assignment;
+
+public class Shapeq9 {
+	
+	protected int side;
+	
+	public Shapeq9() {
+		side=3;
+	}
+	
+	public int getSide() {
+		return side;
+	}
+	
+	public void printSide() {
+		System.out.println("This object have "+side+" sides");
+	}
+
+}
