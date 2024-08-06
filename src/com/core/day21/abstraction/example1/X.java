988868224f436aa1f8e13364bee0a0dc0eaccae5
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-08-06 23:43:15 +0530

    Day 21 abstraction example1

---
+++ src/com/core/day21/abstraction/example1/X.java
@@ -0,0 +1,17 @@
+package com.core.day21.abstraction.example1;
+
+public class X extends A{
+	
+	void m1() {
+		System.out.println("x m1");
+	}
+	
+	void m2(int a, int b) {
+		System.out.println(a+b);
+	}
+	
+	void m3() {
+		System.out.println("x m3");
+	}
+
+}
