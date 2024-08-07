3229fb55d5ad416603d0b3644dbdbea853c3696b
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-08-07 22:19:22 +0530

    Day 21 abstraction Ex2 Test

---
+++ src/com/core/day21/abstraction/assignment/AbstractTestEx2.java
@@ -0,0 +1,12 @@
+package com.core.day21.abstraction.assignment;
+
+public class AbstractTestEx2 {
+	
+	public static void main(String[] args) {
+		AbstractSubclassEx2 obj=new AbstractSubclassEx2();
+		obj.get(10, 20);
+		obj.add();
+		obj.display();
+	}
+
+}
