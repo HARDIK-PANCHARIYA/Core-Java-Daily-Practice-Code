55d70e20776da8e06b7e8cae2db552a4b695f859
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-08-07 22:18:37 +0530

    Day 21 abstraction Ex2

---
+++ src/com/core/day21/abstraction/assignment/AbstractclassEx2.java
@@ -0,0 +1,11 @@
+package com.core.day21.abstraction.assignment;
+
+public abstract class AbstractclassEx2 {
+	
+	abstract void get(int a, int b);
+	
+	abstract void add();
+	
+	abstract void display();
+
+}
