05ba2598f93d3f27fee7af03f286260cafded633
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-08-07 22:16:58 +0530

    Day 21 abstraction Ex1

---
+++ src/com/core/day21/abstraction/assignment/AbstractclassEx1.java
@@ -0,0 +1,11 @@
+package com.core.day21.abstraction.assignment;
+
+public abstract class AbstractclassEx1 {
+	
+	public void disp1() {
+		System.out.println("Concrete method of abstract class");
+	}
+	
+	abstract public void disp2();
+
+}
