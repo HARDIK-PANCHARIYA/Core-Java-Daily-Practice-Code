73676c1509f04eea77be3e0fe579ae8b6dd1b01e
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-08-08 23:32:41 +0530

    Day 21 abstraction Ex3

---
+++ src/com/core/day21/abstraction/assignment/AbstractclassEx3.java
@@ -0,0 +1,9 @@
+package com.core.day21.abstraction.assignment;
+
+public abstract class AbstractclassEx3 extends AbstractEx3{
+	
+	abstract void getCity(String city);
+	
+	abstract void getCountry(String country);
+
+}
