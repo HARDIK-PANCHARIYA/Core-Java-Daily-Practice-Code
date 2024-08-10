e56539391603f61dc1c36e1f53dbbc4119147e1c
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-08-10 23:33:57 +0530

    Day 21 abstraction Ex4

---
+++ src/com/core/day21/abstraction/assignment/AbstractTestEx4.java
@@ -0,0 +1,16 @@
+package com.core.day21.abstraction.assignment;
+
+public class AbstractTestEx4 {
+	
+	public static void main(String[] args) {
+		
+	AbstractSubclassEx4 obj=new AbstractSubclassEx4();
+	
+	obj.getVal("Hardik");
+	obj.display();
+	
+	}
+	
+	
+
+}
