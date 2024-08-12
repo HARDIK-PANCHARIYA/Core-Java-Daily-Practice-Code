f4d5fbedffa3c9bd198fd3151befadaff9276f71
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-08-12 22:01:27 +0530

    Day 21 abstraction Interfaces X class test

---
+++ src/com/core/day21/abstraction/interfaces/Xtest.java
@@ -0,0 +1,15 @@
+package com.core.day21.abstraction.interfaces;
+
+public class Xtest {
+	
+	public static void main(String[] args) {
+		
+		X obj=new X();
+		
+		obj.m1();
+		obj.m2();
+		obj.m3();
+		
+	}
+
+}
