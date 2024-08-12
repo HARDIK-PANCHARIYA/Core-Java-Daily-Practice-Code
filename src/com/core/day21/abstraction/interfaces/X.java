a208348e450aadbe1405d19a5ab1474d7b3eb4ef
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-08-12 22:01:06 +0530

    Day 21 abstraction Interfaces X class

---
+++ src/com/core/day21/abstraction/interfaces/X.java
@@ -0,0 +1,17 @@
+package com.core.day21.abstraction.interfaces;
+
+public class X implements I1,I2{
+	
+	public void m1() {
+		System.out.println("m1");
+	}
+	
+	public void m2() {
+		System.out.println("m2");
+	}
+	
+	public void m3() {
+		System.out.println("m3");
+	}
+
+}
