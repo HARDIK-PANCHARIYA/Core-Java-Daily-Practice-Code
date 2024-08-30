feea9af4422c79ba2ee639ad06a5232bc334e18e
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-08-30 23:23:55 +0530

    Day 28 pattern Practice

---
+++ src/com/core/day20pattern/q7.java
@@ -0,0 +1,17 @@
+package com.core.day20pattern;
+
+public class q7 {
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5; j++) {
+			System.out.print("("+i+","+j+")");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
