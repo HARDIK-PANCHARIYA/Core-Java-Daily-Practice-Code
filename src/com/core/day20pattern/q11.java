902cd92cf0689575b9a25ad3fc806836abe76da6
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-08-31 23:48:56 +0530

    Day 28 pattern Practice

---
+++ src/com/core/day20pattern/q11.java
@@ -0,0 +1,15 @@
+package com.core.day20pattern;
+
+//  Print table of 17
+
+public class q11 {
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10; i++) {
+			System.out.println("17"+"*"+i+"="+17*i);
+		}
+		
+	}
+
+}
