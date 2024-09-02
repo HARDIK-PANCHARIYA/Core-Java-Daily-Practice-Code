fa8e831d3ccac310fce2039067398bb19e445c3a
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-02 23:27:59 +0530

    Day 28 pattern Practice

---
+++ src/com/core/pattern/Pattern2.java
@@ -0,0 +1,26 @@
+package com.core.pattern;
+
+/*
+
+*  
+* *  
+* * *  
+* * * *  
+* * * * * 
+
+*/
+
+public class Pattern2 {
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
