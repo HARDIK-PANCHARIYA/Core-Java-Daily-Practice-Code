54a349797f48156b50dd84744063c7ef5e1b7ead
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-12 20:53:42 +0530

    Logic

---
+++ src/com/core/Logic/P4.java
@@ -0,0 +1,14 @@
+package com.core.Logic;
+
+// Print number from 1 t0 10
+
+public class P4 {
+	
+	public static void main(String[] args) {
+	
+	for(int i=1; i<=10; i++) {
+		System.out.println(i);
+	}
+	}
+
+}
