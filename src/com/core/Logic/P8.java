75d2fc602d545072cb1dff414e73765296b02922
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-13 16:32:23 +0530

    Logic

---
+++ src/com/core/Logic/P8.java
@@ -0,0 +1,15 @@
+package com.core.Logic;
+
+// print the sum of 1 to 10
+
+public class P8 {
+	
+	int sum() {
+		int sum=0;
+		for(int i=1; i<=10; i++) {
+			sum=sum+i;
+		}
+		return(sum);
+	}
+
+}
