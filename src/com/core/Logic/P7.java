4a952291e0e6b2276b89b19137bc5b6d45df7f6c
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-13 16:31:54 +0530

    Logic

---
+++ src/com/core/Logic/P7.java
@@ -0,0 +1,17 @@
+package com.core.Logic;
+
+// print odd number form 66 to 44
+
+public class P7 {
+	
+	public static void main(String[] args) {
+		
+		for(int i=66; i>=44; i--) {
+			if(i%2!=0) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
