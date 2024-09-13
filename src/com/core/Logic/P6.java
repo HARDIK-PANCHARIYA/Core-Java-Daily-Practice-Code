22e24a8408bbd5cfb63d5a606f35ca960a622b10
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-13 16:31:25 +0530

    Logic

---
+++ src/com/core/Logic/P6.java
@@ -0,0 +1,17 @@
+package com.core.Logic;
+
+// print even number from 15 to 35
+
+public class P6 {
+	
+	public static void main(String[] args) {
+		
+		for(int i=15; i<=35; i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
