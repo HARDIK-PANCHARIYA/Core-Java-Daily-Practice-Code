fd3288a799605b40c0e2e7127171eccf2dd3d461
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-14 23:49:53 +0530

    Logic

---
+++ src/com/core/Logic/P11.java
@@ -0,0 +1,18 @@
+package com.core.Logic;
+
+//print the number from 1 to 100 - divisible by 7
+
+public class P11 {
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=100; i++) {
+			if(i%7==0) {
+				System.out.println(i);
+			}
+		}
+		
+		
+	}
+
+}
