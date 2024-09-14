ace4b79ddad6d42b142e527fdcb2d61cc1a9cb8d
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-14 23:48:58 +0530

    Logic

---
+++ src/com/core/Logic/P8test.java
@@ -0,0 +1,12 @@
+package com.core.Logic;
+
+public class P8test {
+	
+	public static void main (String args[]) {
+		
+		P8 obj=new P8();
+		System.out.println("Sum of 1 to 10 = "+obj.sum());
+		
+	}
+
+}
