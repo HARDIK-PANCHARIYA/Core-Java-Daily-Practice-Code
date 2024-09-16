ae110f550e1c9f8bc339f60bf645a66eaa20294b
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-16 19:09:19 +0530

    Logic

---
+++ src/com/core/Logic/P20.java
@@ -0,0 +1,20 @@
+package com.core.Logic;
+
+public class P20 {
+	
+	public static void main(String[] args) {
+		
+	
+		
+		for(int j=1; j<=3; j++) {
+			
+			for(int k=1; k<=3; k++) {
+				for(int i=65; i<=90; i++) {
+					System.out.print((char)i);
+				}
+				}
+			System.out.println("");
+			}
+			
+			}
+	}
