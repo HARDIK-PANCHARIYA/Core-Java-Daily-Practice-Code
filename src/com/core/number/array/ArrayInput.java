f7ef995be250e4c20d2d1a453a8a9fcce6e6482d
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-20 20:42:56 +0530

    array number logic

---
+++ src/com/core/number/array/ArrayInput.java
@@ -0,0 +1,25 @@
+package com.core.number.array;
+
+import java.util.Scanner;
+
+public class ArrayInput {
+	
+public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int size =sc.nextInt();
+
+		
+		int[] number=new int[size];
+		
+		for(int i=0; i<size; i++) {
+			number[i]=sc.nextInt();
+		}
+		
+		for(int i=0; i<number.length; i++) {
+			System.out.println(number[i]);
+		}
+		
+	}
+
+}
