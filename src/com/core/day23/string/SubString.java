e757e6b2d5341e742eb0913a0c1a93cf2f7290bf
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-10-19 18:53:31 +0530

    String Practice

---
+++ src/com/core/day23/string/SubString.java
@@ -0,0 +1,15 @@
+package com.core.day23.string;
+
+public class SubString {
+	
+public static void main(String[] args) {
+
+	String s1="HardikPanchariya";
+	
+	System.out.println("S1 before = "+s1);
+	
+	System.out.println("s1 after = ");
+	System.out.println(s1.substring(0,6)+" "+s1.substring(6));
+	
+	}
+}
