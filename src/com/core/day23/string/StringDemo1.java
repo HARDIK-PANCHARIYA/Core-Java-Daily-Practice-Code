2856fad71621da829d816a87fe95be003bdcfdc5
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-10-19 18:52:15 +0530

    String Practice

---
+++ src/com/core/day23/string/StringDemo1.java
@@ -0,0 +1,18 @@
+package com.core.day23.string;
+
+public class StringDemo1 {
+	
+	public static void main(String[] args) {
+		
+	String s1="Hello Hardik";
+	String s2="Hello Hardik";
+	
+	if(s1==s2) {
+		System.out.println("Equal");
+	}else {
+		System.out.println("NOT Equals");
+	}
+	
+	}
+
+}
