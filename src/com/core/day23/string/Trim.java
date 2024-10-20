06db5f8d1ef91e41082920defd08ebc44dee1dba
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-10-20 23:32:33 +0530

    String Practice

---
+++ src/com/core/day23/string/Trim.java
@@ -0,0 +1,15 @@
+package com.core.day23.string;
+
+public class Trim {
+	
+	public static void main(String[] args) {
+		
+		String s1="Hardik Panchariya    ";
+		
+		System.out.println(s1+".....");
+		
+		System.out.println(s1.trim()+"......");
+		
+	}
+
+}
