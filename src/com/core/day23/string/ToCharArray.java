b27b5cda7321e8aaa1ad3dc89cd4756156702589
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-10-20 23:31:57 +0530

    String Practice

---
+++ src/com/core/day23/string/ToCharArray.java
@@ -0,0 +1,16 @@
+package com.core.day23.string;
+
+public class ToCharArray {
+	
+	public static void main(String[] args) {
+		
+		String s1= "HardikPancahriya";
+		char[] ch=s1.toCharArray();
+		
+		for(char c:ch) {
+			System.out.println(c);
+		}
+		
+	}
+
+}
